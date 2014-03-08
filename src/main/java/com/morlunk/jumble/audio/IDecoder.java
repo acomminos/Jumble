b3e4dd2d32962bfe52781eda20dac7dc7f49a7a6
Andrew Comminos <andrewcomminos@gmail.com>
2014-03-08 01:12:54 -0800

    Documentation.

--- src/main/java/com/morlunk/jumble/audio/IDecoder.java
+++ src/main/java/com/morlunk/jumble/audio/IDecoder.java
@@ -21,7 +21,30 @@ package com.morlunk.jumble.audio;
  * Created by andrew on 07/03/14.
  */
 public interface IDecoder {
+    /**
+     * Decodes the encoded data provided into float PCM data.
+     * @param input A byte array of encoded data of size inputSize.
+     * @param inputSize The size of the encoded data.
+     * @param output An initialized output array at least frameSize for float PCM data.
+     * @param frameSize The maximum frame size possible.
+     * @return The number of decoded samples.
+     * @throws NativeAudioException if encoding failed.
+     */
     public int decodeFloat(byte[] input, int inputSize, float[] output, int frameSize) throws NativeAudioException;
+
+    /**
+     * Decodes the encoded data provided into short PCM data.
+     * @param input A byte array of encoded data of size inputSize.
+     * @param inputSize The size of the encoded data.
+     * @param output An initialized output array at least frameSize for short PCM data.
+     * @param frameSize The maximum frame size possible.
+     * @return The number of decoded samples.
+     * @throws NativeAudioException if encoding failed.
+     */
     public int decodeShort(byte[] input, int inputSize, short[] output, int frameSize) throws NativeAudioException;
+
+    /**
+     * Deallocates native resources. The decoder must no longer be called after this.
+     */
     public void destroy();
 }
